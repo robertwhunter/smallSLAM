4fb4bfa61d609f8f9dffec0050526de51dfe9b82
robertwhunter <robert.hunter@doctors.org.uk>
2021-12-13 12:29:33 +0000

    Edits to correct file extensions

--- scripts/1_trim_to_uniq.bash
+++ scripts/1_trim_to_uniq.bash
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-awk 'NR%4==2' $1 | sort | uniq -c | sort -nr | cat -n > $1.uniq 
+#### COUNT UNIQUE READS
+
+awk 'NR%4==2' $1 | sort | uniq -c | sort -nr | cat -n > ${1%.*}.uniq 
 
 # awk 'NR%4==2' = filter out sequences only
  # sort = sort file
   # uniq -c = find unique lines and include counts
    # sort -nr = sort in reverse order by count
     # cat -n = add row numbers
+     # {%.*} to strip existing file extension
 
+
+#### REFORMAT FILE
 
 echo "rank,count,sequence" > $1.tmp
 
 awk '{$2=$2};1' $1.uniq | sed 's/ /,/g' >> $1.tmp
-mv $1.tmp $1.uniq
+mv $1.tmp ${1%.*}.uniq
 
 # awk '{$2=$2};1' to collapse multiple spaces into a single space
   # sed 'sed/ /,/g' to convert spaces into commas
--- scripts/3_parents.R
+++ scripts/3_parents.R
@@ -5,8 +5,6 @@ args = commandArgs(trailingOnly = TRUE)
 
 families_to_parents(
     fn_families = args[1],
-#   base_from = "T",
-#   base_to = "C",
     base_from = fn_getbases(args[1])$base_from,
     base_to = fn_getbases(args[1])$base_to,
     SNP_threshold = 0.1
